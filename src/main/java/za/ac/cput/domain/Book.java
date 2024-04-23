69a7d6e6e52329ef34a933efa4330488627fef39
Mkhetyeva <127616754+Mkhetyeva@users.noreply.github.com>
2024-04-23 14:39:52 +0200

    my domain class that is book

--- src/main/java/za/ac/cput/domain/Book.java
+++ src/main/java/za/ac/cput/domain/Book.java
@@ -1,4 +1,92 @@
 package za.ac.cput.domain;
 
 public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+
+    private Book(Builder builder) {
+        this.title = builder.title;
+        this.author = builder.author;
+        this.publicationYear = builder.publicationYear;
+        this.isbn = builder.isbn;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String title;
+        private String author;
+        private int publicationYear;
+        private String isbn;
+
+        public Builder() {}
+
+        public Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder withAuthor(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder withPublicationYear(int publicationYear) {
+            this.publicationYear = publicationYear;
+            return this;
+        }
+
+        public Builder withIsbn(String isbn) {
+            this.isbn = isbn;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(this);
+        }
+    }
 }
--- src/main/java/za/ac/cput/util/Main.java
+++ src/main/java/za/ac/cput/util/Main.java
@@ -1,4 +1,25 @@
 package za.ac.cput.util;
 
+
+import java.awt.print.Book;
+
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book.Builder()
+                .withTitle("The Great Gatsby")
+                .withAuthor("F. Scott Fitzgerald")
+                .withPublicationYear(1925)
+                .withIsbn("978-3-16-148410-0")
+                .build();
+
+        Book book2 = new Book.Builder()
+                .withTitle("To Kill a Mockingbird")
+                .withAuthor("Harper Lee")
+                .withPublicationYear(1960)
+                .withIsbn("978-3-16-148420-0")
+                .build();
+
+        System.out.println(book1);
+        System.out.println(book2);
+    }
 }
